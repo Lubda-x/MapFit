95de341706c663181ec5cb09e92fb5d77c4ee19e
Lubda-x <abdulhameedisah98@gmail.com>
2025-08-12 15:40:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-let map = L.map('map').setView([10.6081277,7.4391133], 19); // AFIT location
+let map = L.map('map', {
+    zoomControl: false,       // hides the + / - buttons
+    scrollWheelZoom: false,   // disables zoom with mouse wheel
+    doubleClickZoom: false,   // disables zoom on double click
+    touchZoom: false  
+            // disables pinch zoom on touchscreens
+}).setView([10.6081277,7.4391133], 19); // AFIT location
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
@@ -15,3 +21,15 @@ let zoomInOut = document.querySelector('.leaflet-top')
 // let attribution1 = document.querySelector('.leaflet-bottom')
 zoomInOut.style.display = 'none'
 // attribution1.style.display = 'none'
+
+
+L.Routing.control({
+    waypoints: [
+        L.latLng(10.6081277,7.4391133), // Start: AFIT Main Gate
+        L.latLng(10.609877148454233, 7.441088525045217)  // End: Some building in AFIT
+    ],
+    routeWhileDragging: true
+}).addTo(map);
+
+// map.setMinZoom(19);
+// map.setMaxZoom(19);
