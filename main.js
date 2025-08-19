e52abcfb9265b57c458e298eb366ef684168c4bd
Lubda-x <abdulhameedisah98@gmail.com>
2025-08-19 17:14:14 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -26,50 +26,34 @@ let map = L.map('map', {
 //   L.marker([10.6081277,7.4391133]).addTo(map)
 //     .bindPopup('Yo! AFIT.')
 //     .openPopup();
+var marker;
 
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        function (position) {
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
 
-
-map.locate({ setView: true, maxZoom: 19 });
-
-function getMyLocation(e){
-
-    // The code below gets the users current position
-    L.marker(e.latlng).addTo(map)
-    .bindPopup("You are here 📍")
-        .openPopup();
-    // L.circle(e.latlng, e.accuracy).addTo(map);
-    let yourCurrentLocation = e.latlng
-
-    
-    
-    // It is the code below that shows the red line(routing machine stuff...)
-    L.Routing.control({
-    waypoints: [
-        L.latLng(yourCurrentLocation.lat,yourCurrentLocation.lng), // Start: AFIT Main Gate
-        L.latLng(10.60991566108269, 7.441094861908177)  // End: Some building in AFIT
-    ],
-        routeWhileDragging: true
-    }).addTo(map);
-
-
-
-    //The code below resets the zoom level caused by the routing machine.
-    map.setMinZoom(19);
-    map.setMaxZoom(19);
-
-    
-
+            // If marker doesn't exist, create it
+            if (!marker) {
+                marker = L.marker([lat, lng]).addTo(map)
+                    .bindPopup("You are here 📍").openPopup();
+                map.setView([lat, lng], 19); // zoom to location
+            } else {
+                marker.setLatLng([lat, lng]); // move marker
+            }
+        },
+        function (error) {
+            alert("Unable to retrieve location: " + error.message);
+        },
+        {
+            enableHighAccuracy: true, // use GPS if available
+            maximumAge: 0
+        }
+    );
+} else {
+    alert("Geolocation is not supported by your browser.");
 }
-
-map.on('locationfound', getMyLocation)
-
-
-console.log(getMyLocation())
-
-map.on('locationerror', function() {
-    alert("Location access denied or unavailable.");
-});
-
 
 
 // THis block of code removes the zoom in button
